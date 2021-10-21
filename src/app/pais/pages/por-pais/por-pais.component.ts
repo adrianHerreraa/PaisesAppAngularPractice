875f0257566e46ef7223c6aaf03eeeef3593b5d3
AdrianHerrera_ <neomach55@gmail.com>
2021-10-21 01:44:14 -0500

    servicio, interfaz

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { PaisService } from '../../services/pais.service';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-por-pais',
@@ -11,37 +12,37 @@ export class PorPaisComponent {
 
   termino: string = '';
   errorExist: boolean = false;
+  paisesFromTable: Country[] = [];
 
   constructor( private paisService: PaisService ){}
 
-
-  buscar() {
-    console.log(this.termino);
-    this.errorExist = false;
-
-    this.paisService.buscarPais( this.termino )
-      .subscribe( (paises) => {
-        console.log(paises);
-        //this.paises = paises;
-        console.log(paises);
-      }, (err) => {
-        this.errorExist = true;
-        console.log(err);
-        //this.paises   = [];
-      });
-
-  }
-
-  /*buscar(){
+  buscar(){
     this.errorExist = false;
     console.log(this.termino);
 
     this.paisService.buscarPais(this.termino)
-      .subscribe( resp => {
-        console.log(resp);
-      }, err => {
+      .subscribe( (paises) => {
+        console.log(paises);
+        this.paisesFromTable = paises;
+
+        if(paises.length <= 0){
+          this.errorExist = true;
+          this.paisesFromTable = [];
+        }
+
+        /*if(paises.values.length <= 0){
+          this.errorExist = true;
+          this.paisesFromTable = [];
+        }*/
+
+        /*if(resp.status == 404){
+          console.log('Parche de error por Kilian');
+          this.errorExist = true;
+        } */
+      }, (err) => {
         this.errorExist = true;
+        this.paisesFromTable = [];
       });
-  } */
+  }
 
 }
--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,20 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Country } from '../interfaces/pais.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisService {
 
-  private apiURL: String = 'https://restcountries.com/v2';
+  private apiUrl: String = 'https://restcountries.com/v2';
   //private apuURL: String = 'https://restcountries.com/v2/name/mexico';
 
   constructor( private http: HttpClient ) { }
 
-  buscarPais( termino:String ): Observable<any> {
-    const url = `${this.apiURL}/name/${termino}`;
-    return this.http.get(url);
+  buscarPais( termino:String ): Observable<Country[]> {
+    const url = `${ this.apiUrl }/name/${ termino }`;
+    return this.http.get<Country[]>(url);
   }
 
 }
